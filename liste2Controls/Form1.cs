66a6ec4b81708b0a137e9c7ad414473e38eb5d12
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-04-03 13:55:21 +0300

    Add project files.

---
+++ liste2Controls/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace liste2Controls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public ListOfStrings los = new ListOfStrings();
+        public ListOfStrings los2 = new ListOfStrings();
+        public ListOfStrings los3 = new ListOfStrings();
+        public ListOfStrings los4 = new ListOfStrings();
+        public ListOfStrings los5 = new ListOfStrings();
+        public ListOfStrings los6 = new ListOfStrings();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = DateTime.Now.ToString();
+            for (int i = 0; i < 10; i++) los.addStrings(DateTime.Now.Millisecond.ToString());
+            los.printListToForm(this);
+
+            for (int i = 0; i < 10; i++) los2.addStrings(DateTime.Now.Millisecond.ToString());
+            los2.positiony += 20;
+            los2.printListToForm(this);
+
+            for (int i = 0; i < 10; i++) los3.addStrings(DateTime.Now.Millisecond.ToString());
+            los3.positiony += 40;
+            los3.printListToForm(this);
+
+            for (int i = 0; i < 10; i++) los4.addStrings(DateTime.Now.Millisecond.ToString());
+            los4.positiony += 60;
+            los4.printListToForm(this);
+
+            for (int i = 0; i < 10; i++) los5.addStrings(DateTime.Now.Millisecond.ToString());
+            los5.positiony += 80;
+            los5.printListToForm(this);
+
+            for (int i = 0; i < 10; i++) los6.addStrings(DateTime.Now.Millisecond.ToString());
+            los6.positiony += 100;
+            los6.printListToForm(this);
+        }
+    }
+}
---
+++ liste2Controls/ListOfStrings.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace liste2Controls
+{
+
+    public class ListOfStrings
+    {
+        public int capacity = 0;
+        public List<string> lista = new List<string>();
+        public ListOfStrings() { }
+        public int positionx = 10;
+        public int positiony = 10;
+        public void addStrings(string s) { lista.Add(s); capacity++; }
+        public string printElementOfLista(int index)
+        {
+            return lista[index];
+
+        }
+        public string printList()
+        {
+            string s="";
+            for (int i = 0; i < capacity; i++)
+            {
+
+                s += lista[i];
+                s += ",";
+
+            }
+            return s;
+        }
+        public void printListToForm(Form f)
+        {
+            string s = "";
+
+            int counter = f.Controls.Count;
+            for (int i = 0; i < capacity; i++)
+            {
+                s = lista[i];
+                f.Controls.Add(new TextBox());
+                  counter++;
+                f.Controls[counter-1].Text = s;
+                f.Controls[counter-1].Left = positionx * (i + 1) * 10;
+                f.Controls[counter-1].Top = positiony ;
+             
+
+            }
+            
+        }
+
+        public string printListToString(Form f)
+        {
+            string s="";
+          
+           
+            for (int i = 0; i < capacity; i++)
+            {
+
+                s += lista[i];
+              
+               
+
+
+            }
+            return s;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
